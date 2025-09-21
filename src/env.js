26ffaf9dfc83ab6a01006a488b4ef7993b927246
itzshakeeb <itzshakeeb@gmail.com>
2025-09-21 23:18:26 +0600

    [added] : env validation

---
+++ src/env.js
@@ -0,0 +1,57 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    AUTH_SECRET:
+      process.env.NODE_ENV === "production"
+        ? z.string()
+        : z.string().optional(),
+
+    NODE_ENV: z
+      .enum(["development", "test", "production"])
+      .default("development"),
+
+    // NextAuth settings
+    AUTH_TRUST_HOST: z.string().optional(),
+    NEXTAUTH_URL: z.string(),
+  },
+
+  /**
+   * Client-side env vars
+   * Must be prefixed with NEXT_PUBLIC_
+   */
+  client: {
+    // NEXT_PUBLIC_SERVER_FILE_UPLOAD_URL: z.string().url(),
+    // NEXT_PUBLIC_CLIENT_URL: z.string(),
+    // NEXT_PUBLIC_SERVER_URL: z.string(),
+  },
+
+  /**
+   * Map actual process.env vars
+   */
+  runtimeEnv: {
+    AUTH_SECRET: process.env.AUTH_SECRET,
+    NODE_ENV: process.env.NODE_ENV,
+
+    AUTH_TRUST_HOST: process.env.AUTH_TRUST_HOST,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+
+
+    // Client
+    // NEXT_PUBLIC_SERVER_FILE_UPLOAD_URL:
+    //   process.env.NEXT_PUBLIC_SERVER_FILE_UPLOAD_URL,
+    // NEXT_PUBLIC_CLIENT_URL: process.env.NEXT_PUBLIC_CLIENT_URL,
+    // NEXT_PUBLIC_SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL,
+  },
+
+  /**
+   * Skip validation in Docker or CI builds
+   */
+  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
+
+  /**
+   * Treat empty strings as undefined
+   */
+  emptyStringAsUndefined: true,
+});
